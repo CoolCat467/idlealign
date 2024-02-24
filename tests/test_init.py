983146a4c436b2e14d0116f7a0fc8a933dfe965f
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-02-24 16:25:46 -0600

    Make `test_init.py` use functions

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,9 +2,14 @@
 
 import idlealign
 
-assert hasattr(idlealign, "idlealign")
-assert idlealign.__title__ == "idlealign"
-assert hasattr(idlealign, "check_installed")
-assert callable(
-    idlealign.check_installed,
-)
+
+def extension_exists() -> None:
+    assert hasattr(idlealign, "idlealign")
+    assert idlealign.__title__ == "idlealign"
+
+
+def check_installed_exists() -> None:
+    assert hasattr(idlealign, "check_installed")
+    assert callable(
+        idlealign.check_installed,
+    )
